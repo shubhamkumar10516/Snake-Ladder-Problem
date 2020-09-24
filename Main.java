c924ce699c0460f9fee5aed19c92faf143b863b9
shubhamkumar10516 <33356820+shubhamkumar10516@users.noreply.github.com>
2020-09-24 13:38:57 +0530

    [Shubham]REFACTORED:comments added

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main{
  public static void main(String args[]){
 
   int position = 0;
-  int diceRoll = 1 + (int)(Math.Random()*10)%6;
+  // 1 added to avoid zero
+  int diceRoll = 1 + (int)(Math.floor(Math.random()*10))%6;
 
  }
 } 

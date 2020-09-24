d9eb3ba2010d8f4dc22d4d2b3b68bfad6a47bef3
shubhamkumar10516 <33356820+shubhamkumar10516@users.noreply.github.com>
2020-09-24 13:43:18 +0530

    [Shubham]REFACTORED: comments added

--- Main.java
+++ Main.java
@@ -12,8 +12,8 @@ public class Main{
   // 0 for noPlay, 1 for ladder, 2 for snake
   while(position < 100){
 
-   possiblity = (Math.Random()*10)%3;
-   diceRoll = 1 + (int)(Math.Random()*10)%6;
+   possiblity = (int)(Math.floor(Math.Random()*10))%3;
+   diceRoll = 1 + (int)(Math.floor(Math.Random()*10))%6;
    noPlay = 0;
    ladder = diceRoll;
    snake = diceRoll;

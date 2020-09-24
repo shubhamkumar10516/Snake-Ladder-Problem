4201a838126aa316b8db106a4ab53fdfa0f21505
shubham <shubhamkumar10516@gmail.com>
2020-09-24 11:56:52 +0530

    [Shubham]ADDED: UC2 dice roll added as random number generator

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
  public static void main(String args[]){
 
   int position = 0;
-  System.out.println("Welcome to snake ladder problem");
+  int diceRoll = 1 + (int)(Math.Random()*10)%6;
 
  }
 } 

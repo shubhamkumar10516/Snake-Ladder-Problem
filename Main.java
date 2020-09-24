01d189c1c177eb9cb52897097b524255382899fe
shubham <shubhamkumar10516@gmail.com>
2020-09-24 12:04:29 +0530

    [Shubham]ADDED: UC3 file having options of no option , ladder and snake added

--- Main.java
+++ Main.java
@@ -3,7 +3,22 @@ public class Main{
  public static void main(String args[]){
 
   int position = 0;
+  // 0 for noPlay, 1 for ladder, 2 for snake or options
+  int possiblity = (Math.Random()*10)%3;
   int diceRoll = 1 + (int)(Math.Random()*10)%6;
+  int noPlay = 0;
+  int ladder = diceRoll;
+  int snake = diceRoll;
+ 
+  if(possiblity == 0) 
+      postion = position;
+  else if(possiblity == 1)
+      position = position+ladder;
+  else
+      position = position - snake;
+
+  System.out.println("current position : "+ position); 
+  
 
  }
 } 
